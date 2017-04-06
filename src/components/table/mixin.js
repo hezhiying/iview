74d5ee3decb8b35befa313632bad5d7507fbd090
H <21418592@qq.com>
2017-04-06 16:43:37 +0800

    fix table width issue

--- src/components/table/mixin.js
+++ src/components/table/mixin.js
@@ -21,7 +21,7 @@ export default {
             let width = '';
             if (column.width) {
                 width = column.width;
-            } else if (this.columnsWidth[column._index]) {
+            } else if (this.columnsWidth[column._index] && column.width !== undefined) {
                 width = this.columnsWidth[column._index].width;
             }
             // when browser has scrollBar,set a width to resolve scroll position bug
